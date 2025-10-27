4d7e8ecd3f0467ab4b7c202fa7755450ffd0fd58
Ca-mP <ckpatterson1s@capetigers.com>
2025-10-27 13:32:11 -0500

    reworked player to be characterbody3d

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,8 +1,12 @@
-extends RigidBody3D
+extends CharacterBody3D
 
-var mouse_sensitivity := 0.001
+@export var mouse_sensitivity : float
+@export var movement_speed: int
+
 var twist_input := 0.0
 var pitch_input := 0.0
+
+const gravity := 9.8
 
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
@@ -13,15 +17,27 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	
+	#MOVEMENT
 	var input := Vector3.ZERO
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_foward", "move_back")
 	
-	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
+	var forward = %Camera3D.global_basis.z
+	var right = %Camera3D.global_basis.x
+	
+	var movement_vector = (forward * input.z) + (right * input.x)
+	
+	velocity = movement_vector * movement_speed
+	
+	if not is_on_floor():
+		velocity.y -= gravity
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
+	
+	
+	#CAMERA
+	
 	twist_pivot.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
 	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, 
@@ -30,6 +46,8 @@ func _process(delta: float) -> void:
 	)
 	twist_input = 0.0
 	pitch_input = 0.0
+	
+	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
